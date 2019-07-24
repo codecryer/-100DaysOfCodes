4832f03fb0adda677261534170b586739b8f78f9
codecryer <samuel.nzekwe2050@gmail.com>
2019-07-24 23:06:23 +0100

    Day 3

--- index.js
+++ index.js
@@ -13,27 +13,60 @@
 
 //OBJECT;
 
-let person = {
-  name: "samuel",
-  surName: "nzekwe",
-  age: 21
-};
-console.log(person); //{ name: 'samuel', surName: 'nzekwe', age: 21 }----output result
+// let person = {
+//   name: "samuel",
+//   surName: "nzekwe",
+//   age: 21
+// };
+// console.log(person); //{ name: 'samuel', surName: 'nzekwe', age: 21 }----output result
 
-console.log(person.name); //samuel----outputs the value of the name property. the dot notation
-console.log(person["age"]); // 21 ----output  the square bracket notation
+// console.log(person.name); //samuel----outputs the value of the name property. the dot notation
+// console.log(person["age"]); // 21 ----output  the square bracket notation
 
 // ARRAYS;
-let myArray = ["mosh", 100, "days", "of", "codes"];
-console.log(myArray); //[ 'mosh', 100, 'days', 'of', 'codes' ]---output.
+// let myArray = ["mosh", 100, "days", "of", "codes"];
+// console.log(myArray); //[ 'mosh', 100, 'days', 'of', 'codes' ]---output.
 
-console.log(myArray.length); //returns 5 as my array length.
+// console.log(myArray.length); //returns 5 as my array length.
 
-console.log(myArray[2]); // days.
+// console.log(myArray[2]); // days.
 
-//FUNCTIONS
+// //FUNCTIONS
 
-function getMessage() {
-  console.log("i just completed my DAY 2 projections, hurray!");
-}
-getMessage(); // calling the function getMessage prints i just completed my DAY 2 projections, hurray! to the console.
+// function getMessage() {
+//   console.log("i just completed my DAY 2 projections, hurray!");
+// }
+// getMessage(); // calling the function getMessage prints i just completed my DAY 2 projections, hurray! to the console.
+
+// Day 3 **** Operators.
+
+// let x = 3;
+// let y = 4;
+
+// console.log(x + y);
+// console.log(x * y);
+// let incomeLevel = true;               //output is true
+// let onJob = true;
+// let grantLoan = onJob && incomeLevel;
+// console.log(grantLoan);
+
+//conditionals, assignments, variables, ternary...................
+// let incomeLevel = false;
+// let onJob = true;
+// let grantLoan = onJob && incomeLevel;
+// let loanStatus = grantLoan ? console.log("granted") : console.log("denied");
+
+// console.log(loanStatus);
+//please, these logic seems to work well, but my console comes with an Undefined after consoling the right message. what might possibly be thr reason. for one, based on my studies on variables, it means a declared variable hasnt been assigned since all declared but unassigned variables always seem to have 'undefined' as their default values.//
+
+// let x = 1;
+// console.log(++x);
+// console.log(x++);
+let a = "red";
+let b = "blue";
+let c = a; //swapping variable values challenge.
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
